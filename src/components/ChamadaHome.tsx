a7e2aae9b73b1b6b1781110ceafadd3f91bdb627
Caio dos Anjos <coa.anjos@gmail.com>
2023-04-17 13:34:03 -0300

    Cria esqueleto inicial mais tela home

---
+++ src/components/ChamadaHome.tsx
@@ -0,0 +1,64 @@
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export function ChamadaHome() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />      
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Bem-vindo ao Cuide
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+                Agenda muito corrida para marcar procedimentos de estética? Sem
+                problemas! Com o Cuide, você pode procurar encaixes de emergência
+                na agenda de salões.              
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Agendamentos</Button>
+              <Button variant="outlined">Sobre</Button>
+            </Stack>
+          </Container>
+        </Box>
+      </main>
+    </ThemeProvider>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,6 @@
+import Card from "@mui/material/Card";
+import { ChamadaHome } from "../components/ChamadaHome";
+
+export function Home() {
+    return <ChamadaHome />
+}
